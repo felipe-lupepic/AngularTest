1f11085e9b726691a1572b793486b3976ae4674b
Felipe Lupepic <felipe.lupepic@benner.com.br>
2015-03-21 00:54:16 -0300

    Exemplo 2 e 3

---
+++ Exemplo3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Exemplo3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var model = new ExemploViewModel() { Id = 10, Name = "Teste name", Duration = "15m", Speaker = "Speaker teste", Venue = "Venue test" };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Exemplo3/Models/ExemploViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exemplo3.Models
+{
+    public class ExemploViewModel
+    {
+        [Display(Name="Identificador")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Display(Name = "Speaker")]
+        public string Speaker { get; set; }
+
+        [Display(Name = "Venue")]
+        public string Venue { get; set; }
+
+        [Display(Name = "Duration")]
+        public string Duration { get; set; }
+    }
+}
